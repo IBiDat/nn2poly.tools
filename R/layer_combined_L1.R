b34f2d81e4ae6c330a341678029f831508a9fe9b
moralapablo <moralapablo@gmail.com>
2023-07-14 11:18:12 +0200

    Fixed problem with newer versions of tensorflow and deleted unused vignette

--- R/layer_combined_L1.R
+++ R/layer_combined_L1.R
@@ -10,7 +10,7 @@ Layer_Combined_L1 = R6::R6Class(
 		},
 		build = function(input_shape) {
 			self$combined_w_b = self$add_weight(
-				shape = shape(tail(input_shape, 1) + tf$ones(shape(1, 1), dtype = "int32"), self$units),
+				shape = shape(tensorflow::as_tensor(tail(input_shape, 1)) + tf$ones(shape(1, 1), dtype = "int32"), self$units),
 				initializer = "random_normal",
 				trainable = TRUE,
 				# Custom defined constraint with L1 norm
